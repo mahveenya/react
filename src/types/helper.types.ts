27f8cadac4c39bc4fb58d0e14a1d683c79f5583c
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2025-09-24 00:05:49 +0300

    refactor: remove useless utils

--- src/types/helper.types.ts
+++ src/types/helper.types.ts
@@ -1,5 +1,1 @@
-export type Entries<T> = {
-  [K in keyof T]: [K, T[K]];
-}[keyof T][];
-
 export type Validator<T> = (value: unknown) => value is T;
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,6 @@
 export const isEmptyArray = (array: unknown) =>
   Array.isArray(array) && array.length == 0;
 
-export const getEndOfUrlPath = (url: string) =>
-  url.split('/').filter(Boolean).at(-1);
-
 export const toArray = <T>(input: T) => {
   if (!input) return [];
   return Array.isArray(input) ? input : [input];
